d67cd61c949e728b9f331e915128772fbb34464b
abhimanyu <ak0922008@gmail.com>
2024-04-12 16:46:19 +0530

    genrated html for products in checkout.html by js

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,11 @@
-export const cart  = [];
+export const cart  = [{
+  productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+  productQuantity: 2,
+},
+{
+ productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+ productQuantity: 1 
+}];
 
 export function addToCart(productId, productQuantity) {
     let matchingProduct;
