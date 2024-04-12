5b8555a5f9205fd50b19abd724d93959efe269f4
abhimanyu <ak0922008@gmail.com>
2024-04-12 22:14:12 +0530

    made delete button interactive in checkout.html

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,11 +1,10 @@
-import { cart } from "../data/cart.js";
+import { cart, removeFromCart } from "../data/cart.js";
 import { products } from "../data/products.js";
 import { formatCurrency } from "./utils/money.js";
 
-
-let cartSummaryHtml;
+let cartSummaryHtml = "";
 cart.forEach((cartItem) => {
-  const { productId, productQuantity} = cartItem;
+  const { productId, productQuantity } = cartItem;
 
   let matchingProduct;
   products.forEach((product) => {
@@ -14,7 +13,9 @@ cart.forEach((cartItem) => {
     }
   });
   cartSummaryHtml += `
-         <div class="cart-item-container">
+         <div class="cart-item-container js-cart-item-container-${
+           matchingProduct.id
+         }">
             <div class="delivery-date">
               Delivery date: Tuesday, June 21
             </div>
@@ -37,7 +38,9 @@ cart.forEach((cartItem) => {
                   <span class="update-quantity-link link-primary">
                     Update
                   </span>
-                  <span class="delete-quantity-link link-primary">
+                  <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${
+                    matchingProduct.id
+                  }">
                     Delete
                   </span>
                 </div>
@@ -92,5 +95,13 @@ cart.forEach((cartItem) => {
         `;
 });
 
+document.querySelector(".js-order-summary").innerHTML = cartSummaryHtml;
 
-document.querySelector('.js-order-summary').innerHTML = cartSummaryHtml; 
+document.querySelectorAll(".js-delete-link").forEach((link) => {
+  link.addEventListener("click", () => {
+    const { productId } = link.dataset;
+    removeFromCart(productId);
+
+    document.querySelector(`.js-cart-item-container-${productId}`).remove();
+  });
+});
