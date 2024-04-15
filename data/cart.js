fd67db365ce93f34ddb137d4b37934cd4fc42626
abhimanyu <ak0922008@gmail.com>
2024-04-15 21:41:09 +0530

    object version of cart

--- data/cart.js
+++ data/cart.js
@@ -1,17 +1,23 @@
-export let cart  =  JSON.parse(localStorage.getItem('cart'));
+export let cart ;
 
-if (!cart){
-  cart = [{
-    productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-    productQuantity: 2,
-    delivaryOptionId: '1'
-  },
-  {
-   productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-   productQuantity: 1 ,
-   delivaryOptionId: '2'
-  }];
-  
+loadFormStorage();
+
+function loadFormStorage() {
+  cart = JSON.parse(localStorage.getItem('cart'));
+
+  if (!cart){
+    cart = [{
+      productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+      productQuantity: 2,
+      delivaryOptionId: '1'
+    },
+    {
+     productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+     productQuantity: 1 ,
+     delivaryOptionId: '2'
+    }];
+    
+  }
 }
 
 
